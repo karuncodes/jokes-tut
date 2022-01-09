f424a3f98cf508927cdfcff820034f338375f054
Karun Kamal <karundeveloper@gmail.com>
2022-01-10 00:36:47 +0530

    db installed, seeded and plugged to UI

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,29 @@
+import type { LoaderFunction } from "remix";
+import { Link, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+import type { Joke } from "@prisma/client";
+
+type LoaderData = { joke: Joke };
+
+export const loader: LoaderFunction = async ({
+  params
+}) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId }
+  });
+  if (!joke) throw new Error("Joke not found");
+  const data: LoaderData = { joke };
+  return data;
+};
+
+
 export default function JokeRoute() {
+  const data: LoaderData = useLoaderData();
+  const { name, content } = data.joke;
     return (
       <div>
-        <p>Here's your hilarious joke:</p>
-        <p>
-          Why don't you find hippopotamuses hiding in trees?
-          They're really good at it.
-        </p>
+        <p>{name}</p>
+        <p>{content}</p>
       </div>
     );
   }
